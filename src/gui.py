82f169bbb9e1da0ff2d2212ab6ce33ec5e92c395
MarkTheArk <60559094+MarkTheArk@users.noreply.github.com>
2023-04-30 17:30:59 -0400

    Update gui.py

--- src/gui.py
+++ src/gui.py
@@ -1,24 +1,100 @@
-!apt-get install -y xvfb
-import os
 import tkinter as tk
-import tkinter.ttk as ttk
+from tkinter import ttk
 from tkinter.filedialog import askopenfile
-os.system("Xvfb :1 -screen 0 720x720x16 &")
-os.environ['DISPLAY'] = ":1.0"
 
-##GUI Initializer
-window = tk.Tk()
-window.geometry('200x100')
-greeting = tk.Label(text="Please Upload an image file containing the text you wish to compare(Acceptable formats are JPG, PNG, etc.)",           
-    width=10,
-    height=5)
-
-greeting.pack
 def open_file():
-    file = askopenfile(mode ='r', filetypes =[('Image Files', '*.png'),('Image Files', '*.JPG')])
+        file = askopenfile(mode ='r', filetypes =[('Image Files', '*.png'),('Image Files', '*.JPG')])
+        get_char(file)
  
-btn = tk.Button(window, text ='Choose File', command = lambda:open_file())
-btn.pack(pady = 10)
+LARGEFONT =("Verdana", 35)
 
-window.mainloop()
-##code for file upload button that will then send to the image identifier method
+class MainApp(tk.Tk):
+     
+    # __init__ function for class tkinterApp
+    def __init__(self, *args, **kwargs):
+         
+        # __init__ function for class Tk
+        tk.Tk.__init__(self, *args, **kwargs)
+         
+        # creating a container
+        container = tk.Frame(self) 
+        container.pack(side = "top", fill = "both", expand = True)
+  
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
+  
+        # initializing frames to an empty array
+        self.frames = {} 
+  
+        # iterating through a tuple consisting
+        # of the different page layouts
+        for F in (StartPage, Results):
+  
+            frame = F(container, self)
+  
+            # initializing frame of that object from
+            # startpage, page1, page2 respectively with
+            # for loop
+            self.frames[F] = frame
+  
+            frame.grid(row = 0, column = 0, sticky ="nsew")
+  
+        self.show_frame(StartPage)
+  
+    # to display the current frame passed as
+    # parameter
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+ 
+  
+# first window frame startpage
+  
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+         
+        # label of frame Layout 2
+        label = ttk.Label(self, text ="Handwriting Comparator", font = LARGEFONT)
+    
+        # putting the grid in its place by using
+        # grid
+        label.grid(row = 1, column = 2, padx = 10, pady = 10)
+        
+        greeting = tk.Label(self, text="Please Upload an image file containing the text you wish to compare(Acceptable formats are JPG, PNG, etc.)")
+        greeting.grid(row = 2, column = 2, padx = 10, pady = 10)
+  
+        button1 = ttk.Button(self, text ="Open File",
+        command = lambda : [open_file(), controller.show_frame(Results)])
+     
+        # putting the button in its place by
+        # using grid
+        button1.grid(row = 3, column = 2, padx = 10, pady = 10)
+  
+          
+  
+  
+# second window frame page1
+class Results(tk.Frame):
+        
+    def __init__(self, parent, controller):
+         
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text ="Results", font = LARGEFONT)
+        label.grid(row = 1, column = 2, padx = 10, pady = 10)
+          
+        # button to show frame 2 with text
+        # layout2
+        button1 = ttk.Button(self, text ="Return to Menu",
+                            command = lambda : controller.show_frame(StartPage))
+     
+        # putting the button in its place
+        # by using grid
+        button1.grid(row = 3, column = 2, padx = 10, pady = 10)
+        
+        
+  
+  
+# Driver Code
+app = MainApp()
+app.mainloop()
